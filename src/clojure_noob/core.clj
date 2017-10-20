4c73a2ecea0fdaac8c3cb84631272f4a2006ac3a
Imre Bartis <imre.bartis@siili.com>
2017-10-20 10:24:19 +0300

    maps & vectors added

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -22,3 +22,104 @@
       "By Aquaman's trident!"))
 ; => Success! ( printed in the REPL)
 ; => "By Zeus's hammer!" ( returned as the value of the entire if expression)
+
+(when true
+  (println "Success!")
+  "abra cadabra")
+; => Success! ( printed in the REPL)
+; => "abra cadabra" ( returned as the value of the entire when expression)
+
+(nil? 1)
+; => false
+
+(nil? nil)
+; => true
+
+(if "bears eat beets"
+  "bears beets Battlestar Galactica")
+; => "bears beets Battlestar Galactica"
+
+(if nil
+  "This won't be the result because nil is falsey"
+  "nil is falsey")
+; => "nil is falsey"
+
+(or false nil :large_I_mean_venti :why_cant_I_just_say_large)
+; => :large_I_mean_venti
+
+(or (= 0 1) (= "yes" "no"))
+; => false
+
+(or nil)
+; => nil
+
+(and :free_wifi :hot_coffee)
+; => :hot_coffee (returns the last truthy value)
+
+(and :feelin_super_cool nil false)
+; => nil (returns the first falsey value)
+
+(def failed-protagonist-names
+  ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"])
+
+failed-protagonist-names
+; => ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"]
+
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+       (if (= severity :mild)
+         "MILDLY INCONVENIENCED!"
+         "DOOOOOOOMED!")))
+
+(error-message :mild)
+; => "OH GOD! IT'S A DISASTER! WE'RE MILDLY INCONVENIENCED!"
+
+; MAPS
+(hash-map :a 1 :b 2)
+; => {:a 1 :b 2}
+
+(get {:a 0 :b 1} :b)
+; => 1
+
+(get {:a 0 :b {:c "ho hum"}} :b)
+; => {:c "ho hum"}
+
+(get {:a 0 :b 1} :c)
+; => nil
+
+(get {:a 0 :b 1} :c "unicorns?")
+; => "unicorns?" ("unicorns?" here is the default value)
+
+; the get-in function lets you look up values in nested maps:
+(get-in {:a 0 :b {:c "ho hum"}} [:b :c])
+; => "ho hum"
+
+; Another way to look up a value in a map is to treat
+; the map like a function with the key as its argument:
+({:name "The Human Coffeepot"} :name)
+; => "The Human Coffeepot"
+
+(:a {:a 1 :b 2 :c 3})
+; => 1
+
+(get {:a 1 :b 2 :c 3} :a)
+; => 1
+
+(:d {:a 1 :b 2 :c 3} "No gnome knows homes like Noah knows")
+; => "No gnome knows homes like Noah knows" (this is the default value here)
+
+
+; VECTORS
+(get [3 2 1] 0)
+; => 3
+
+(get ["a" {:name "Pugsley Winterbottom"} "c"] 1)
+; => {:name "Pugsley Winterbottom"}
+
+(vector "creepy" "full" "moon")
+; => ["creepy" "full" "moon"]
+
+(conj [1 2 3] 4)
+; => [1 2 3 4]
+
