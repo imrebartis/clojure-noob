c50c2bb18ffd82adf9d47e200b4d6b8a25dbfa60
Imre Bartis <imre.bartis@siili.com>
2017-10-21 14:02:45 +0300

    function body added

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -302,3 +302,27 @@ failed-protagonist-names
 (announce-treasure-location {:lat 28.22 :lng 81.33})
 ; => Treasure lat: 28.22
 ; => Treasure lng: 81.33
+
+; FUNCTION BODY
+
+;Clojure automatically returns the last form evaluated:
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "joe")
+
+(illustrative-function)
+; => "joe"
+
+(defn number-comment
+  [x]
+  (if (> x 6)
+    "Oh my gosh! What a big number!"
+    "That number's OK, I guess"))
+
+(number-comment 5)
+; => "That number's OK, I guess"
+
+(number-comment 7)
+; => "Oh my gosh! What a big number!"
